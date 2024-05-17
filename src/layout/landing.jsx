cf318368853e9f95c96e9c08dbd8d18ebec23b97
joaomluis <jooluis2@gmail.com>
2024-05-17 14:59:56 +0100

    added: sign up landing page

--- src/layout/landing.jsx
+++ src/layout/landing.jsx
@@ -1,16 +1,20 @@
-import { Container, Row, Col } from "reactstrap";
 import MainNavBar from "../components/navbar/navbar.jsx";
 import LandingPageContent from "../views/landingPageContent.jsx";
+
+
 
 function Landing() {
   return (
     <div className="wrapper">
       <MainNavBar />
 
-      <div className="main-panel" style={{ marginTop: "90px" }}>
-        <LandingPageContent />
+
+        <div className="main-panel" style={{ marginTop: "65px" }}>
+          <div className="section1">
+            <LandingPageContent />
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
 
