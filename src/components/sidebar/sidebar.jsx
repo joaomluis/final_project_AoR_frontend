c3b6043fd9db4f887788639b5f692f63c4dd5e19
joaomluis <jooluis2@gmail.com>
2024-05-24 11:20:57 +0100

    added: sidebar component

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
 
-function Sidebar() {
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
+function SideNavbar() {
   return (
-    <Nav className="col-md-2 d-none d-md-block bg-secondary sidebar"  style={{marginTop:"65px", height: "calc(100vh - 65px)", display: "flex", flexDirection: "column", justifyContent: "flex-end"}}>
-      <div className="sidebar-sticky"></div>
-      <Nav.Item>
-        <Nav.Link href="/home">Home</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/about">About</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/contact">Contact</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    
+
+    <Sidebar style={{marginTop:"65px", height: "calc(100vh - 65px)"}}>
+      <Menu>
+        <SubMenu label="Charts">
+          <MenuItem> Pie charts </MenuItem>
+          <MenuItem> Line charts </MenuItem>
+        </SubMenu>
+        <MenuItem> Documentation </MenuItem>
+        <MenuItem> Calendar </MenuItem>
+      </Menu>
+    </Sidebar>
   );
 }
 
-export default Sidebar;
+export default SideNavbar;
